cf09387c4a5c4491f014af1463f382e52d9d4147
tomito26 <tommybwah@gmail.com>
2020-07-31 20:28:42 +0300

    added a new project

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let slideshowImages = document.querySelectorAll('.intro .slideshow-img');
+let nextImageDelay = 3000;
+let currentImageCounter = 0;
+
+slideshowImages[currentImageCounter].style.opacity = 1;
+
+setInterval(nextImage,nextImageDelay);
+
+
+function nextImage(){
+    // slideshowImages[currentImageCounter].style.opacity = 0;
+    slideshowImages[currentImageCounter].style.zIndex = -2; 
+    const tempCounter = currentImageCounter;
+    setTimeout(() =>{
+        slideshowImages[tempCounter].style.opacity = 0;
+    },100);
+    
+    currentImageCounter =(currentImageCounter + 1) % slideshowImages.length;
+    slideshowImages[currentImageCounter].style.opacity = 1;
+    slideshowImages[currentImageCounter].style.zIndex = -1;
+}
